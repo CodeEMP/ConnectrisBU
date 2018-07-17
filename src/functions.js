b308a6eac129c75a035a02bb3edf92fda200da09
CodeEMP <cpgameface@gmail.com>
2018-07-17 14:56:23 -0500

    no back

--- src/functions.js
+++ src/functions.js
@@ -223,6 +223,29 @@ export function checkForTee(state,x,y,playerColor,alt) {
   return state;
 }
 
+export function checkForEll(state,x,y,playerColor,alt) {
+  if (x+1<=8&&y+1<=8&&y+2<=8&&(state.squares[x][y+1].color===playerColor||state.squares[x][y+1].color===alt||state.squares[x][y+1].color==='Purple')&&
+  (state.squares[x][y+2].color===playerColor||state.squares[x][y+2].color===alt||state.squares[x][y+2].color==='Purple')&&
+  (state.squares[x+1][y+2].color===playerColor||state.squares[x+1][y+2].color===alt||state.squares[x+1][y+2].color==='Purple')) {
+    state = cycleColors(state,x,y);
+    state = cycleColors(state,x,y+1);
+    state = cycleColors(state,x,y+2);
+    state = cycleColors(state,x+1,y+2);
+    state = addPoint(state);
+  }
+  else if (x-1>=0&&y-1>=0&&y-2>=0&&(state.squares[x-1][y-2].color===playerColor||state.squares[x-1][y-2].color===alt||state.squares[x-1][y-2].color==='Purple')&&
+  (state.squares[x-1][y-1].color===playerColor||state.squares[x-1][y-1].color===alt||state.squares[x-1][y-1].color==='Purple')&&
+  (state.squares[x-1][y].color===playerColor||state.squares[x-1][y].color===alt||state.squares[x-1][y].color==='Purple')) {
+    state = cycleColors(state,x,y);
+    state = cycleColors(state,x-1,y-2);
+    state = cycleColors(state,x-1,y-1);
+    state = cycleColors(state,x-1,y);
+    state = addPoint(state);
+  }
+
+
+}
+
 export function addPoint(state) {
   if (state.currentPlayer==='Red') {
     state.redShape = shapeFour();
